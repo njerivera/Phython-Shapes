9c7b3a8f2e18a81a7468deef6a39140091d0320e
njerivera <veronjeri85@gmail.com>
2019-05-02 19:20:48 +0300

    add quiz

---
+++ shapes.py/shapes.py
@@ -0,0 +1,60 @@
+class Circle:
+    def __init__ (self,radius):
+        self.radius =radius 
+
+    def area(self,A):
+        A=πr2
+
+        totalr=A(22/7*r**2)
+        return totalr
+    def circumference(self,C):
+        C=2πr
+
+        totalc=C(2*22/7*r)
+        return totalc   
+        
+class Square:
+    def __init__ (self,side):
+        self.side=side  
+
+    def area (self,A):
+        A=πr2
+
+        land = A(22/7*r**2)
+        return land 
+    def Perimeter(self,P):
+        P = 4a
+        totalp =P(4*a)
+        return totalp   
+
+class Rectangle:
+    def __init__ (self,w,l):
+        self.w=w
+        self.l=l
+
+    def area (self,A):
+        A=wl
+        totala=A(w*l)
+        return totala   
+    def perimeter(self,P):
+        P=2(l+w)
+        totalpp = P*2(l+w)
+        return  totalpp 
+
+    class Sphere:
+        def __init__ (self,r):
+            self.r=r
+        def surface_area(self,A):
+            A=4πr2
+
+            totals =A(4*22/7*r**2)
+            return  totals
+
+
+        def volume(self,V):
+            V = 4/3(πr3)
+            totalv =V(4/3*22/7*r**3)
+            return totalv
+
+
+
