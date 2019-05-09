8960fdb77d2b1309c6ff0dc7e91f01a5faf563b7
njerivera <47848769+njerivera@users.noreply.github.com>
2019-05-09 19:09:32 +0300

    added quiz 2

---
+++ quiz.py
@@ -0,0 +1,44 @@
+def comprehension(a):
+  list = []
+  for x in list:
+    b = x%3
+    list.append(b)
+  return list
+
+def divisible_by_three(n):
+	x=range(1,n)
+	if m %3==0:
+		print(m)
+	elif
+	    m % 3!=0:
+           print(m)
+    return x
+      
+def nested():
+    list=[]
+    for n in list:
+    	append(list)
+
+def duplicate():
+	a=set([])
+
+
+def sorted(a,b,c):
+	d=a+b+c
+	d.sort()
+	return d
+
+def smallest(x):
+	return min(x)
+def student(name,age):
+    student= {"e":"f","g":"h"}
+    students=[]
+    for student in students:
+    	year=2019-(student["age"])
+        message="Hello {} you were born in {}".format(student["name"],year)
+        print(message)
+
+	
+def divisible_by_three(numbers):
+	for number in numbers:
+		print(number%)
